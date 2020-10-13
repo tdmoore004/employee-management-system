543c76c200d07083a4a2e2d14e84cf85d7e132fa
Tanner Moore <tdmoore004@gmail.com>
2020-10-12 20:31:55 -0600

    Added information to connect to MySQL server.

--- app.js
+++ app.js
@@ -2,4 +2,19 @@
 const inquirer = require("inquirer");
 const mysql = require("mysql");
 const consoleTable = require("console.table");
+const dotenv = require("dotenv");
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "",
+    database: "employee_management_db",
+});
+
+connection.connect((err) => {
+    if(err) throw err;
+    console.log("Connected as id " + connection.threadId + "\n");
+    init();
+});
 
