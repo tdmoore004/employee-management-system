571b7c058b474a0691baba2df46592412b62b38d
Tanner Moore <tdmoore004@gmail.com>
2020-10-14 21:03:01 -0600

    Added functions to add and view items.

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // Dependencies
 const inquirer = require("inquirer");
 const mysql = require("mysql");
-const consoleTable = require("console.table");
+const cTable = require("console.table");
 const dotenv = require("dotenv");
 
 dotenv.config();
@@ -39,23 +39,17 @@ const start = () => {
                 inquirer
                     .prompt(addViewWhat).then((response) => {
                         if (response.addViewWhat === "Department") {
-                            start();
+                            addDepartment();
                         } else if (response.addViewWhat === "Role") {
-                            start();
+                            addRole();
                         } else if (response.addViewWhat === "Employee") {
-                            start();
+                            addEmployee();
                         };
                     });
             } else if (response.addOrUpdate === "VIEW") {
                 inquirer
                     .prompt(addViewWhat).then((response) => {
-                        if (response.addViewWhat === "Department") {
-                            start();
-                        } else if (response.addViewWhat === "Role") {
-                            start();
-                        } else if (response.addViewWhat === "Employee") {
-                            start();
-                        };
+                        viewItem(response.addViewWhat.toLowerCase());
                     });
             } else if (response.addOrUpdate === "UPDATE") {
                 inquirer
@@ -70,4 +64,71 @@ const start = () => {
                 connection.end();
             }
         });
+};
+
+const addDepartment = () => {
+    inquirer
+        .prompt({
+            name: "departmentName",
+            type: "input",
+            message: "What is the name of the department you want to add?"
+        }).then((response) => {
+            connection.query("INSERT INTO department (name) VALUES (?)", [response.departmentName], (err, result) => {
+                if (err) throw err;
+            });
+        });
+};
+
+const addRole = () => {
+    inquirer
+        .prompt([{
+            name: "roleTitle",
+            type: "input",
+            message: "What is the title of the role you want to add?"
+        },
+        {
+            name: "roleDepartment",
+            type: "input",
+            message: "What is the ID for the department this role is in?"
+        }]).then((response) => {
+            connection.query("INSERT INTO role (title, department_id) VALUES (?, ?)", [response.roleTitle, response.roleDepartment], (err, result) => {
+                if (err) throw err;
+            });
+        });
+};
+
+const addEmployee = () => {
+    inquirer
+        .prompt([{
+            name: "firstName",
+            type: "input",
+            message: "What is the first name of the employee you are adding?"
+        },
+        {
+            name: "lastName",
+            type: "input",
+            message: "What is the last name of the employee you are adding?"
+        },
+        {
+            name: "roleId",
+            type: "input",
+            message: "What is the ID for the role this employee will have?"
+        },
+        {
+            name: "managerId",
+            type: "input",
+            message: "What is the ID for this employee's manager? (If there is no manager enter 0)"
+        }
+    ]).then((response) => {
+        connection.query("INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)", [response.firstName, response.lastName, response.roleId, response.managerId], (err, result) => {
+            if (err) throw err;
+        });
+    });
+};
+
+const viewItem = (table) => {
+    connection.query("SELECT * FROM " + table, (err, response) => {
+        if (err) throw err;
+        console.table(response);
+    });
 };
